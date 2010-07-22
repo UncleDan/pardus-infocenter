b48a1b3b16412076bf1c72d9e37f7a5401ae58f4
uncledan74 <uncledan74@7cd3571a-2e39-11df-954b-21e382534593>
2010-07-22 21:49:21 +0000

    Tagged userscripts for release 1.5b2.xxx and 1.6+ (forgot files...)

--- infocenter/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-	require_once("global.php");
-	require_once("modules/security_mod.php");
-	SecurityMod::logout();
-?>
--- infocenter/modules/system.php
+++
@@ -1,5 +0,0 @@
-<?php
-	function v($a, $i) {
-		return isset($a[$i]) ? $a[$i] : null;
-	}
-?>
