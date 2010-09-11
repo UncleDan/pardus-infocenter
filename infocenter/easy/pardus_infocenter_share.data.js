c7de05fea998351fe3d56009988d253052a62f4e
bcable <bcable@7cd3571a-2e39-11df-954b-21e382534593>
2010-09-11 23:19:52 +0000

    Oops, overwrote the easy script's config.

--- infocenter/easy/pardus_infocenter_share.data.js
+++ infocenter/easy/pardus_infocenter_share.data.js
@@ -35,13 +35,11 @@ var enablePaymentShare  = true;
 
 var servers = [
 	{
-		name: "PardusInfocenter", //the name you want to be displayed in the combo box
-		url: "http://mysite/infocenter", //the exact url to your Infocenter, no trailing slashes
+		name: "<INFOCENTER_NAME>", //the name you want to be displayed in the combo box
+		url: "<INFOCENTER_URL>", //the exact url to your Infocenter, no trailing slashes
 		accounts: { //the user names you wont to use; to disable a universe, just delete the line, but look twice at commas!!!
 					//ATTENTION: if not using EASY script it is suggested to give to these account(s) permissions to send data only
-			orion: {name: "Orion-Send", password: "Pardus"},
-			artemis: {name: "Artemis-Send", password: "Pardus"},
-			pegasus: {name: "Pegasus-Send", password: "Pardus"}
+			<UNIVERSE>: {name: "<USERNAME>", password: "<PASSWORD>"}
 		}
 	}
 ];
